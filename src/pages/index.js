6bd166972ac383fd718efb9555620ad5ac19099c
gabrieli06 <benitesrosa04@gmail.com>
2024-09-30 15:44:47 +0000

    aula 30/09

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,24 @@
-function Index(){
-    return <h1>Olá Mundo</h1>
+import Footer from "./component/footer";
+import Navbar from "./component/navbar";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+export default function Index() {
+    return <>
+        <div>
+            <Navbar />
+            <Container>
+                <Row>
+                    <Col>1 of 2</Col>
+                    <Col>2 of 2</Col>
+                </Row>
+                <Row>
+                    <Col>1 of 3</Col>
+                    <Col>2 of 3</Col>
+                    <Col>3 of 3</Col>
+                </Row>
+            </Container>
+            <Footer />
+        </div>
+    </>;
 }
-export default Index; 
--- src/pages/noticias/[id].js
+++ src/pages/noticias/[id].js
@@ -1,7 +1,7 @@
-      import { useRouter } from "next/router"
+      import { useRouter } from "next/router";
       export default function Page() {
           const router = useRouter()
           return <>
         <p>Post: {router.query.id}</p>
     </>
-      }
+}
