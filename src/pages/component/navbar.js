4f8b36195ab9d876618aaaa9e1d857a9ba9a3fd7
gabrieli06 <benitesrosa04@gmail.com>
2024-10-07 15:43:05 +0000

    aula 07/10

--- src/pages/component/navbar.js
+++ src/pages/component/navbar.js
@@ -1,3 +1,19 @@
-export default function Navbar(){
-    return <div>Menu</div>
+import {Container, Nav, Navbar} from 'react-bootstrap';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+export default function Menu() {
+  return (
+    <Navbar expand="sm" className="bg-info">
+      <Container>
+        <Navbar.Brand href="/">6B</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
+
